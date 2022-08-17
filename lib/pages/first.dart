a5725e19510ca602e0554a30eaba61fc267ebc5e
Muhammad Yousuf <Muhammadyousufgemini@gmail.com>
2022-08-17 16:41:49 +0500

    main.dart screen ready

---
+++ lib/pages/first.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class First extends StatefulWidget {
+  const First({Key? key}) : super(key: key);
+
+  @override
+  State<First> createState() => _FirstState();
+}
+
+class _FirstState extends State<First> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("First"),),
+    );
+  }
+}

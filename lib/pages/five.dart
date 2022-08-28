687eea925d32a2ae6f00265ecd10c64cda3a01ef
Muhammad Yousuf <Muhammadyousufgemini@gmail.com>
2022-08-28 17:17:14 +0500

    Five screen ready

--- lib/pages/five.dart
+++ lib/pages/five.dart
@@ -101,8 +101,30 @@ class _FiveState extends State<Five>   with SingleTickerProviderStateMixin{
                   controller: _tabController,
                 ),
               ),
+    
+        
             ],
+          
           ),
+        
+        ),
+        Positioned(
+          top:600,
+          child: Container(
+                       width: MediaQuery.of(context).size.width*0.85,
+                       height: MediaQuery.of(context).size.height*0.10,
+                 decoration: BoxDecoration(
+                  
+                  borderRadius: BorderRadius.circular(10),
+                  border: Border.all(
+                    width: MediaQuery.of(context).size.width*0.01,
+                    color: Colors.blue
+                  
+                  )
+                  
+                 ),
+                 child: Center(child: Text("Shop Now",style: TextStyle(fontSize: 40,fontWeight: FontWeight.w500,color: Colors.black,),)),
+               ),
         ),
         ]
       ),
@@ -117,7 +139,7 @@ class _FiveState extends State<Five>   with SingleTickerProviderStateMixin{
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-         Text("MATERIALAS",style: TextStyle(fontWeight: FontWeight.w500,letterSpacing: 1,color: Colors.black),),
+         Text("MATERIALAS",style: TextStyle(fontWeight: FontWeight.w500,letterSpacing: 1,color: Colors.black,fontSize: 16),),
          Padding(
            padding: const EdgeInsets.only(top:5.0),
            child: Text("AS SEEN IN REDBOOK! You'll be printed and ready in the Perfect In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content.  " ,style: TextStyle(fontSize: 15,letterSpacing: 2, color: Color.fromARGB(255, 113, 111, 111)),),
@@ -132,94 +154,3 @@ class _FiveState extends State<Five>   with SingleTickerProviderStateMixin{
 
 }
     
-//     MaterialApp(
-    
-//       debugShowCheckedModeBanner: false,
-//       home: 
-//        Scaffold(
-           
-//         // backgroundColor: Color.fromARGB(255, 165, 234, 201),
-//         appBar: AppBar(
-                 
-//           leading: IconButton(
-//               icon: Icon(Icons.arrow_back),
-//               color: Colors.black,
-//               onPressed: () {
-//                 Navigator.pop(context);
-//               }),
-//           backgroundColor: Colors.white,
-//           title: Text(
-//             'Home Screen',
-//             style: TextStyle(color: Colors.black),
-//           ),
-//         ),
-//         body: Stack(
-//           children: [
-//             Positioned(
-//               height: 400,
-//               width: 360,
-//               child: Container(
-//                 decoration: BoxDecoration(
-//                   image: DecorationImage(
-//                     image: AssetImage("assets/four.jpg"),
-//                     fit: BoxFit.cover,
-//                   ),
-//                 ),
-//               ),
-//             ),
-//             Positioned(
-//                 top: MediaQuery.of(context).size.height * 0.55,
-//                 child: Container(
-//                   child: Column(
-//                     crossAxisAlignment: CrossAxisAlignment.start,
-//                     children: [
-//                       Padding(
-//                         padding: const EdgeInsets.only(left: 10.0),
-//                         child: Text(
-//                           "Perfect Situation Purple Long Sleeve dress",
-//                           style: TextStyle(
-//                               letterSpacing: 1,
-//                               fontSize: 16,
-//                               color: Color.fromARGB(255, 127, 125, 125)),
-//                         ),
-//                       ),
-//                       Padding(
-//                         padding: const EdgeInsets.only(left: 10.0),
-//                         child: Text("Rs.25.99",
-//                             style: TextStyle(
-//                                 color: Color.fromARGB(255, 39, 0, 231),
-//                                 fontSize: 18,
-//                                 fontWeight: FontWeight.w500)),
-//                       ),
-                        
-//                     ],
-//                   ),
-//                 )
-//                 ),
-//                 //  DefaultTabController(
-//                 //   length: 2,
-//                 //   child: Column(children: [TabBar(
-//                 //       unselectedLabelColor: Colors.black,
-//                 //       labelColor: Colors.red,
-//                 //       tabs: <Widget>[
-//                 //         Tab(
-//                 //           icon: Icon(Icons.people),
-//                 //         ),
-//                 //         Tab(
-//                 //           icon: Icon(Icons.person),
-//                 //         )
-//                 //       ],controller: _tabController,
-//                 //       indicatorSize: TabBarIndicatorSize.tab,
-//                 //     ),TabBarView(
-//                 //       children: <Widget>[Text('people'), Text('Person')],
-//                 //       controller: _tabController,
-//                 //     ),
-                      
-//                 //     ])
-//                 //     )
-//           ],
-//         ),
-//            ),
-//     );
-//   }
-// }

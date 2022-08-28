2a4f46146228922472ac0ed14f8739103c722842
Muhammad Yousuf <Muhammadyousufgemini@gmail.com>
2022-08-28 14:51:11 +0500

    Five screen ready

--- lib/pages/five.dart
+++ lib/pages/five.dart
@@ -13,15 +13,36 @@ class _FiveState extends State<Five> {
     return Scaffold(
       // backgroundColor: Color.fromARGB(255, 165, 234, 201),
       appBar: AppBar(
-      
         leading: IconButton(
             icon: Icon(Icons.arrow_back),
             color: Colors.black,
             onPressed: () {
               Navigator.pop(context);
             }),
-          backgroundColor: Colors.white,
-        title: Text('Home Screen',style: TextStyle(color: Colors.black),),
+        backgroundColor: Colors.white,
+        title: Text(
+          'Home Screen',
+          style: TextStyle(color: Colors.black),
+        ),
+      ),
+      body: Stack(
+        
+        children: [
+          Positioned(
+         
+            height: 400,
+            width:360,
+            child: 
+ Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage("assets/four.jpg"),
+              fit: BoxFit.cover,
+            ),
+          ),
+       ),
+          )
+        ],
       ),
     );
   }

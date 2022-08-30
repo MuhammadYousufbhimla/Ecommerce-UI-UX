ed7e35e64859dc69f103939a5c7482bd4aa425b3
Muhammad Yousuf <Muhammadyousufgemini@gmail.com>
2022-08-30 12:59:54 +0500

    Six screen Start

---
+++ lib/pages/six.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Six extends StatefulWidget {
+  const Six({Key? key}) : super(key: key);
+
+  @override
+  State<Six> createState() => _SixState();
+}
+
+class _SixState extends State<Six> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}

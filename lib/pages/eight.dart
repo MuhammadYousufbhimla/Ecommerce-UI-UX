40f31072178c077e62577dc9e4bc5ec7921ade6c
Muhammad Yousuf <84890859+MuhammadYousufbhimla@users.noreply.github.com>
2022-09-08 22:49:56 +0500

    Eight screen aDD

--- lib/pages/eight.dart
+++ lib/pages/eight.dart
@@ -35,148 +35,68 @@ class _SevenState extends State<Eight> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView(children: [
-        Container(
-          margin: EdgeInsets.only(
-            top: MediaQuery.of(context).size.height * 0.02,
-            left: MediaQuery.of(context).size.height * 0.01,
-            right: MediaQuery.of(context).size.height * 0.01,
-          ),
-          decoration: BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.circular(20),
-            boxShadow: [
-              BoxShadow(
-                color: Colors.grey.withOpacity(0.2),
-                spreadRadius: 3,
-                blurRadius: 5,
-                offset: Offset(0, 3), // changes position of shadow
-              ),
-            ],
-          ),
-          child: TextField(
-            autofocus: true,
-            style: TextStyle(fontSize: 15.0, color: Colors.black),
-            decoration: InputDecoration(
-              filled: false,
-              suffixIcon: Icon(Icons.search_rounded),
-              fillColor: Colors.black,
-              hintText: 'Search Cart',
-              contentPadding:
-                  const EdgeInsets.only(left: 14.0, bottom: 12.0, top: 12.0),
-              border: InputBorder.none,
+        body: ListView(children: [
+      Container(
+        margin: EdgeInsets.only(
+          top: MediaQuery.of(context).size.height * 0.02,
+          left: MediaQuery.of(context).size.height * 0.01,
+          right: MediaQuery.of(context).size.height * 0.01,
+        ),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(20),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey.withOpacity(0.2),
+              spreadRadius: 3,
+              blurRadius: 5,
+              offset: Offset(0, 3), // changes position of shadow
             ),
+          ],
+        ),
+        child: TextField(
+          autofocus: true,
+          style: TextStyle(fontSize: 15.0, color: Colors.black),
+          decoration: InputDecoration(
+            filled: false,
+            suffixIcon: Icon(Icons.search_rounded),
+            fillColor: Colors.black,
+            hintText: 'Search Cart',
+            contentPadding:
+                const EdgeInsets.only(left: 14.0, bottom: 12.0, top: 12.0),
+            border: InputBorder.none,
           ),
         ),
-
-        Cart()
-        // Padding(padding: EdgeInsets.only(top: 15.0)),
-        // Container(
-        //     child: GridView.builder(
-        //         physics: NeverScrollableScrollPhysics(),
-        //         shrinkWrap: true,
-        //         gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-        //           crossAxisCount: 2,
-        //           // mainAxisSpacing: 04,
-        //           // crossAxisSpacing: 05,
-        //           mainAxisExtent: 250,
-        //         ),
-        //         itemCount: ImageList.length,
-        //         // itemCount: myProducts.length,
-        //         itemBuilder: (BuildContext ctx, index) {
-        //           return Column(
-        //             children: [
-        //               Container(
-        //                   height: MediaQuery.of(context).size.height * 0.29,
-        //                   margin: EdgeInsets.only(
-        //                       left: MediaQuery.of(context).size.height * 0.02,
-        //                       right: MediaQuery.of(context).size.height * 0.02),
-        //                   // alignment: Alignment.center,
-        //                   decoration: BoxDecoration(
-        //                       color: Colors.white,
-        //                       borderRadius: BorderRadius.circular(10),
-        //                       boxShadow: [
-        //                         BoxShadow(
-        //                           color: Colors.grey.withOpacity(0.2),
-        //                           spreadRadius: 2,
-        //                           blurRadius: 2,
-        //                           offset: Offset(0, 3),
-        //                         )
-        //                       ]),
-        //                   child: Column(
-        //                     crossAxisAlignment: CrossAxisAlignment.start,
-        //                     children: [
-        //                       Center(
-        //                         child: Image.asset(
-        //                           ImageList[index],
-        //                           height:
-        //                               MediaQuery.of(context).size.height * 0.22,
-        //                         ),
-        //                       ),
-        //                       Padding(
-        //                         padding: EdgeInsets.only(
-        //                             left: MediaQuery.of(context).size.width *
-        //                                 0.02),
-        //                         child: Row(
-        //                           mainAxisAlignment:
-        //                               MainAxisAlignment.spaceBetween,
-        //                           children: [
-        //                             Text(
-        //                               "Rs:499.9",
-        //                               style: TextStyle(
-        //                                   color:
-        //                                       Color.fromARGB(255, 39, 0, 231),
-        //                                   fontWeight: FontWeight.w500),
-        //                             ),
-        //                             Padding(
-        //                               padding: EdgeInsets.only(right: 5.0),
-        //                               child: Icon(
-        //                                 Icons.favorite,
-        //                                 color: Color.fromARGB(255, 39, 0, 231),
-        //                               ),
-        //                             )
-        //                           ],
-        //                         ),
-        //                       ),
-        //                       Padding(
-        //                         padding: EdgeInsets.only(
-        //                             left: MediaQuery.of(context).size.width *
-        //                                 0.02),
-        //                         child: Text(
-        //                           Imagename[index],
-        //                           style: TextStyle(color: Colors.grey),
-        //                         ),
-        //                       )
-        //                       // Text(myProducts[index]["name"]),
-        //                       // Padding(
-        //                       //   padding: const EdgeInsets.only(top: 20.0),
-        //                       //   child: Text(
-        //                       //     "Rs:25.99",
-        //                       //     style: TextStyle(
-        //                       //       fontWeight: FontWeight.w500,
-        //                       //       color: Color.fromARGB(255, 39, 0, 231),
-        //                       //     ),
-        //                       //   ),
-        //                       // )
-        //                     ],
-        //                   )),
-        //             ],
-        //           );
-        //         }))
-      ]),
-    );
+      ),
+      Cart(context, ImageList[0]),
+      Cart(context, ImageList[1]),
+      Cart(context, ImageList[2]),
+      Cart(context, ImageList[3]),
+      Cart(context, ImageList[4]),
+      Cart(context, ImageList[5]),
+      Cart(context, ImageList[6]),
+      paymentbtn(context)
+    ]));
   }
 
-  Widget Cart() {
+  Widget Cart(context, leadImage) {
     return Container(
-      height: MediaQuery.of(context).size.height * 0.29,
+      // height: MediaQuery.of(context).size.height * 0.29,
       margin: EdgeInsets.only(
-          left: MediaQuery.of(context).size.height * 0.02,
-          right: MediaQuery.of(context).size.height * 0.02),
-      // alignment: Alignment.center,
+        left: MediaQuery.of(context).size.height * 0.02,
+        right: MediaQuery.of(context).size.height * 0.02,
+        top: MediaQuery.of(context).size.height * 0.02,
+        bottom: MediaQuery.of(context).size.height * 0.02,
+      ),
+      padding: EdgeInsets.only(
+        left: MediaQuery.of(context).size.height * 0.02,
+        right: MediaQuery.of(context).size.height * 0.02,
+        top: MediaQuery.of(context).size.height * 0.02,
+        bottom: MediaQuery.of(context).size.height * 0.02,
+      ),
       decoration: BoxDecoration(
           color: Colors.white,
-          borderRadius: BorderRadius.circular(10),
+          borderRadius: BorderRadius.circular(8),
           boxShadow: [
             BoxShadow(
               color: Colors.grey.withOpacity(0.2),
@@ -185,7 +105,99 @@ class _SevenState extends State<Eight> {
               offset: Offset(0, 3),
             )
           ]),
-      child: Text("data"),
+      child: ListTile(
+        minLeadingWidth: 10,
+        leading: Image.asset(
+          leadImage,
+        ),
+        title: Text(
+          "Hawaian Shirt",
+          style: TextStyle(color: Colors.black, fontSize: 13),
+        ),
+        subtitle: Text(
+          "SANDY WILLIAM",
+          style: TextStyle(color: Colors.grey, fontWeight: FontWeight.w500),
+        ),
+        trailing: Text(
+          "Rs:499.9",
+          style: TextStyle(
+              color: Color.fromARGB(255, 39, 0, 231),
+              fontWeight: FontWeight.w500),
+        ),
+      ),
+    );
+  }
+
+  Widget paymentbtn(context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Container(
+          margin: EdgeInsets.only(
+            left: MediaQuery.of(context).size.height * 0.02,
+            right: MediaQuery.of(context).size.height * 0.02,
+            top: MediaQuery.of(context).size.height * 0.02,
+            bottom: MediaQuery.of(context).size.height * 0.02,
+          ),
+          padding: EdgeInsets.only(
+            left: MediaQuery.of(context).size.height * 0.02,
+            right: MediaQuery.of(context).size.height * 0.02,
+            top: MediaQuery.of(context).size.height * 0.02,
+            bottom: MediaQuery.of(context).size.height * 0.02,
+          ),
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(8),
+
+            // boxShadow: [
+            //   BoxShadow(
+            //     color: Colors.grey.withOpacity(0.2),
+            //     spreadRadius: 2,
+            //     blurRadius: 2,
+            //     offset: Offset(0, 3),
+            //   )
+            // ]
+          ),
+          child: Text(
+            "Total : Rs 5000",
+            style: TextStyle(
+              color: Color.fromARGB(255, 39, 0, 231),
+            ),
+          ),
+        ),
+        Container(
+          width: MediaQuery.of(context).size.width * 0.40,
+          // height: MediaQuery.of(context).size.height * 0.20,
+          margin: EdgeInsets.only(
+            left: MediaQuery.of(context).size.height * 0.02,
+            right: MediaQuery.of(context).size.height * 0.02,
+            top: MediaQuery.of(context).size.height * 0.02,
+            bottom: MediaQuery.of(context).size.height * 0.02,
+          ),
+          padding: EdgeInsets.only(
+            left: MediaQuery.of(context).size.height * 0.02,
+            right: MediaQuery.of(context).size.height * 0.02,
+            top: MediaQuery.of(context).size.height * 0.02,
+            bottom: MediaQuery.of(context).size.height * 0.02,
+          ),
+          decoration: BoxDecoration(
+              color: Color.fromARGB(255, 39, 0, 231),
+              borderRadius: BorderRadius.circular(8),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.grey.withOpacity(0.2),
+                  spreadRadius: 2,
+                  blurRadius: 2,
+                  offset: Offset(0, 3),
+                )
+              ]),
+          child: Text(
+            "Pay Now",
+            textAlign: TextAlign.center,
+            style: TextStyle(fontWeight: FontWeight.w500, color: Colors.white),
+          ),
+        )
+      ],
     );
   }
 }

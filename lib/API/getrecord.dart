09ff84b49dee10013cc8f52e20b9f0efcbeedc7c
Muhammad Yousuf <Muhammadyousufgemini@gmail.com>
2022-09-12 12:34:23 +0500

    APi call

---
+++ lib/API/getrecord.dart
@@ -0,0 +1,43 @@
+import 'dart:convert';
+import 'package:flutter_easyloading/flutter_easyloading.dart';
+import 'package:http/http.dart' as http;
+getmembers() async {
+  // DialogHelper.showLoading('Fetching data');
+
+  try {
+    final response = await http
+        .get(Uri.parse('https://jsonplaceholder.typicode.com/posts'));
+    var jsonData = jsonDecode(response.body);
+    print(" Json Data ${jsonData.length}");
+    print(" vList final object${jsonData}");
+    // print(jsonData[0][0]['service_name']);
+    List<Calldata> calldatascreen = [];
+    // print(" calldatascreen : $calldatascreen");
+    for (var i = 0; i < jsonData.length; i++) {
+      Calldata servvices = Calldata(
+        id: jsonData[i]['id'],
+        title: jsonData[i]['title'],
+        body: jsonData[i]['body'],
+       
+      );
+      calldatascreen.add(servvices);
+    }
+
+    print("List final is ${calldatascreen[0].title}");
+    // DialogHelper.hideLoading();
+    // EasyLoading.dismiss();
+
+    return calldatascreen;
+  } catch (e) {
+    print("   error catch $e");
+  }
+}
+
+class Calldata {
+  String? id;
+  String? title;
+  String? body;
+
+
+  Calldata({this.id, this.title, this.body, });
+}
